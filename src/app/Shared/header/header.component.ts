af44a660ecfafda6299e1d6b02b26886d4c3e5cf
Deepak Kumar <aitdeepak11624@gmail.com>
2020-05-09 17:02:16 +0530

    expand-collapse feature enhanced

--- src/app/Shared/header/header.component.ts
+++ src/app/Shared/header/header.component.ts
@@ -17,7 +17,7 @@ export class HeaderComponent implements OnInit {
       this.states = data
       console.log(this.states);
       this.last =  this.states.cases_time_series[this.states.cases_time_series.length - 1]
-      
+      console.log("this.last",this.last)
 
     });
   
